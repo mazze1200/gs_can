b05903a3939e7f4d96988a9c844bb315de6f48b5
Mathias Bösl <mathias.boesl@gmx.de>
2024-03-24 12:33:29 +0100

    make it work with green LED of the nucleo-H723ZG

--- src/bin/low_level_timer_api.rs
+++ src/bin/low_level_timer_api.rs
@@ -38,86 +38,80 @@ async fn main(_spawner: Spawner) {
 
     info!("Hello World!");
 
-    let mut pwm = SimplePwm32::new(p.TIM5, p.PA0, p.PA1, p.PA2, p.PA3, khz(10));
+    let mut pwm = SimplePwm32::new(p.TIM3, p.PB0, khz(10));
     let max = pwm.get_max_duty();
-    pwm.enable(Channel::Ch1);
+    pwm.enable(Channel::Ch3);
 
     info!("PWM initialized");
     info!("PWM max duty {}", max);
 
     loop {
-        pwm.set_duty(Channel::Ch1, 0);
+        info!("Reload");
+
+        pwm.set_duty(Channel::Ch3, 0);
         Timer::after_millis(300).await;
-        pwm.set_duty(Channel::Ch1, max / 4);
+        pwm.set_duty(Channel::Ch3, max / 4);
         Timer::after_millis(300).await;
-        pwm.set_duty(Channel::Ch1, max / 2);
+        pwm.set_duty(Channel::Ch3, max / 2);
         Timer::after_millis(300).await;
-        pwm.set_duty(Channel::Ch1, max - 1);
+        pwm.set_duty(Channel::Ch3, max - 1);
         Timer::after_millis(300).await;
     }
 }
-pub struct SimplePwm32<'d, T: CaptureCompare32bitInstance> {
+pub struct SimplePwm32<'d, T: CaptureCompare16bitInstance> {
     inner: PeripheralRef<'d, T>,
 }
 
-impl<'d, T: CaptureCompare32bitInstance> SimplePwm32<'d, T> {
+impl<'d, T: CaptureCompare16bitInstance> SimplePwm32<'d, T> {
     pub fn new(
         tim: impl Peripheral<P = T> + 'd,
-        ch1: impl Peripheral<P = impl Channel1Pin<T>> + 'd,
-        ch2: impl Peripheral<P = impl Channel2Pin<T>> + 'd,
         ch3: impl Peripheral<P = impl Channel3Pin<T>> + 'd,
-        ch4: impl Peripheral<P = impl Channel4Pin<T>> + 'd,
         freq: Hertz,
     ) -> Self {
-        into_ref!(tim, ch1, ch2, ch3, ch4);
+        into_ref!(tim,  ch3);
 
         T::enable_and_reset();
 
-        ch1.set_speed(Speed::VeryHigh);
-        ch1.set_as_af(ch1.af_num(), AFType::OutputPushPull);
-        ch2.set_speed(Speed::VeryHigh);
-        ch2.set_as_af(ch1.af_num(), AFType::OutputPushPull);
+
         ch3.set_speed(Speed::VeryHigh);
-        ch3.set_as_af(ch1.af_num(), AFType::OutputPushPull);
-        ch4.set_speed(Speed::VeryHigh);
-        ch4.set_as_af(ch1.af_num(), AFType::OutputPushPull);
+        ch3.set_as_af(ch3.af_num(), AFType::OutputPushPull);
 
-        let mut this = Self { inner: tim };
+        
+        let this = Self { inner: tim };
 
-        this.set_frequency(freq);
+        this.inner.set_frequency(freq);
         this.inner.start();
 
-        let r = T::regs_gp32();
-        r.ccmr_output(0)
-            .modify(|w| w.set_ocm(0, OutputCompareMode::PwmMode1.into()));
-        r.ccmr_output(0)
-            .modify(|w| w.set_ocm(1, OutputCompareMode::PwmMode1.into()));
+        let r = T::regs_gp16();
+        // r.ccmr_output(0)
+        //     .modify(|w| w.set_ocm(0, OutputCompareMode::PwmMode1.into()));
+        // r.ccmr_output(0)
+        //     .modify(|w| w.set_ocm(1, OutputCompareMode::PwmMode1.into()));
+
+        // This enables the output for channel 3
         r.ccmr_output(1)
             .modify(|w| w.set_ocm(0, OutputCompareMode::PwmMode1.into()));
-        r.ccmr_output(1)
-            .modify(|w| w.set_ocm(1, OutputCompareMode::PwmMode1.into()));
+
+        // r.ccmr_output(1)
+        //     .modify(|w| w.set_ocm(1, OutputCompareMode::PwmMode1.into()));
 
         this
     }
 
     pub fn enable(&mut self, channel: Channel) {
-        T::regs_gp32().ccer().modify(|w| w.set_cce(channel.index(), true));
+        T::regs_gp16().ccer().modify(|w| w.set_cce(channel.index(), true));
     }
 
     pub fn disable(&mut self, channel: Channel) {
-        T::regs_gp32().ccer().modify(|w| w.set_cce(channel.index(), false));
+        T::regs_gp16().ccer().modify(|w| w.set_cce(channel.index(), false));
     }
 
-    pub fn set_frequency(&mut self, freq: Hertz) {
-        <T as embassy_stm32::timer::low_level::GeneralPurpose32bitInstance>::set_frequency(&mut self.inner, freq);
+    pub fn get_max_duty(&self) -> u16 {
+        T::regs_gp16().arr().read().arr()
     }
 
-    pub fn get_max_duty(&self) -> u32 {
-        T::regs_gp32().arr().read()
-    }
-
-    pub fn set_duty(&mut self, channel: Channel, duty: u32) {
+    pub fn set_duty(&mut self, channel: Channel, duty: u16) {
         defmt::assert!(duty < self.get_max_duty());
-        T::regs_gp32().ccr(channel.index()).write_value(duty)
+        T::regs_gp16().ccr(channel.index()).write( |val| val.set_ccr(duty));
     }
 }
