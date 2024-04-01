4358489580d36a8b722318050944749d980a3ec0
Mathias Bösl <mathias.boesl@gmx.de>
2024-04-01 14:36:52 +0200

    update scripts

--- send_fd.sh
+++ send_fd.sh
@@ -1,4 +1,8 @@
+#!/bin/bash
+# link https://munich.dissec.to/kb/chapters/can/canfd.html
+
 while true
 do
-	cansend can0 222#0102030405060708
+	# cansend can0 222##6010203040506070809
+	cansend can0 444##1deadbeefdeadbeefdeadbeefdeadbeef  # Send a 16 bytes CAN FD message with an higher bitrate for the data
 done
